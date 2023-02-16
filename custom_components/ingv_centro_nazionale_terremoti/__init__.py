3f9385c5b8e02dba27357cb8ac9ed0ed06fc5852
caiosweet <24454580+caiosweet@users.noreply.github.com>
2023-02-16 07:51:54 +0100

    migrate INGV to use async_forward_entry_setups
    Fixed #22

--- custom_components/ingv_centro_nazionale_terremoti/__init__.py
+++ custom_components/ingv_centro_nazionale_terremoti/__init__.py
@@ -87,7 +87,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     _LOGGER.debug("Feed entity coordinator added for %s", entry.entry_id)
 
     async_cleanup_entity_registry(hass=hass, entry=entry)
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
